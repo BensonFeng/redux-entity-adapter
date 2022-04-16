e820a9321c37a23d61d2df929f7ede4d78fa7ac5
BensonFeng <bensonfeng@outlook.com>
2022-04-16 19:46:17 -0400

    creating delete action w/ entityadapter and create async thunk

--- features/commentsSlice.js
+++ features/commentsSlice.js
@@ -13,17 +13,28 @@ export const fetchComments = createAsyncThunk(
   }
 );
 
+export const deleteComment = createAsyncThunk(
+  "comments/deleteComment",
+  async (id) => {
+    await fetch(`https://jsonplaceholder.typicode.com/comments/${id}`, {
+      method: "DELETE",
+    });
+    return id;
+  }
+);
+
 const commentsAdapter = createEntityAdapter({
   selectId: (comment) => comment.id,
 });
 
 const commentsSlice = createSlice({
   name: "comments",
-  initialState: commentsAdapter.getInitialState({
-    loading: false,
-    replica: [],
-  }),
-  reducers: {},
+  initialState: commentsAdapter.getInitialState({ loading: false }),
+  reducers: {
+    setAllComments: commentsAdapter.setAll,
+    setOneComments: commentsAdapter.removeOne,
+    setManyComments: commentsAdapter.addMany,
+  },
   extraReducers: {
     [fetchComments.pending](state) {
       state.loading = true;
@@ -31,10 +42,19 @@ const commentsSlice = createSlice({
     [fetchComments.fulfilled](state, { payload }) {
       state.loading = false;
       commentsAdapter.setAll(state, payload);
-      state.replica = payload;
     },
     [fetchComments.rejected](state) {
       state.loading = false;
+    },
+    [deleteComment.rejected](state) {
+      state.loading = false;
+    },
+    [deleteComment.pending](state) {
+      state.loading = true;
+    },
+    [deleteComment.fulfilled](state, { payload: id }) {
+      state.loading = false;
+      commentsAdapter.removeOne(state, id);
     },
   },
 });
@@ -42,5 +62,8 @@ const commentsSlice = createSlice({
 export const commentsSelectors = commentsAdapter.getSelectors(
   (state) => state.comments
 );
+
+export const { setAllComments, setManyComments, setOneComments } =
+  commentsSlice.actions;
 
 export default commentsSlice.reducer;
--- features/components/Comment.js
+++ features/components/Comment.js
@@ -1,11 +1,11 @@
 import { Panel, Button, ButtonToolbar } from "rsuite";
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, onDelete }) => {
   return (
-    <Panel header={comment.name} bordered style={{ margin: 20 }}>
+    <Panel header={<h1>{comment.id}</h1>} bordered style={{ margin: 20 }}>
       {comment.body}
       <ButtonToolbar style={{ marginTop: 10 }}>
-        <Button size="lg" color="red">
+        <Button size="lg" color="red" onClick={() => onDelete(comment.id)}>
           Delete
         </Button>
         <Button size="lg" color="cyan">
